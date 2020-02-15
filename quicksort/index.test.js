dc1a337e4f4d526556bec3447e18016e3d95d115
Cloudswillopenforme <Christine.Tikhonowa@gmail.com>
2020-02-15 12:55:10 +0300

    tests splittet

--- quicksort/index.test.js
+++ quicksort/index.test.js
@@ -1,13 +1,16 @@
 import {quicksort, inplace_quicksort} from './index.js';
 
-test('test quicksort functions', () => {
-
 const arr = [1, 4, 2, 7, 12, 0, -9, 22];
 const sortedArr = [-9, 0, 1, 2, 4, 7, 12, 22];
 
+test('test quicksort functions', () => {
+
     expect(quicksort(arr))
         .toStrictEqual(sortedArr);
-
-    expect(inplace_quicksort(arr, 0, arr.length - 1))
-        .toStrictEqual(sortedArr);
 })
+
+test('test inplace-quicksort functions', () => {
+    
+        expect(inplace_quicksort(arr, 0, arr.length - 1))
+            .toStrictEqual(sortedArr);
+    })
