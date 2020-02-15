720eaec0cfe51f97ffdb274c8245294686be213f
Cloudswillopenforme <Christine.Tikhonowa@gmail.com>
2020-02-15 12:33:44 +0300

    quicker inplace implementation added

--- quicksort/index.js
+++ quicksort/index.js
@@ -1,15 +1,53 @@
+// divide and conquer recursive algorithm
+
+// book example, slow implementation
+// take first element as pivot
+// can degrade from O(nlogn) to O(n^2) 
 const quicksort = (array) => {
-    if (array.length < 2) {
-      return array;
-    } else {
-      let pivot = array[0];
-      let less = array.slice(1).filter(el => el <= pivot);
-      let greater = array.slice(1).filter(el => el > pivot);
-  
-      return quicksort(less).concat([pivot], quicksort(greater));
+  if (array.length < 2) {
+    return array;
+  } else {
+    let pivot = array[0];
+    let less = array.slice(1).filter(el => el <= pivot);
+    let greater = array.slice(1).filter(el => el > pivot);
+
+    return quicksort(less).concat([pivot], quicksort(greater));
+  }
+}
+
+// in-place quicksort implementation
+// take last element as pivot
+// about O(nlogn) average
+const inplace_quicksort = (array, start, end) => {
+
+  if (start >= end) return;
+
+  const index = partition(array, start, end);
+  inplace_quicksort(array, start, index - 1);
+  inplace_quicksort(array, index + 1, end);
+
+  return array;
+}
+
+const partition = (array, start, end) => {
+
+  let pivotIndex = start;
+  let pivotValue = array[end];
+
+  for (let i = start; i < end; i++) {
+    if (array[i] < pivotValue) {
+      swap(array, i, pivotIndex);
+      pivotIndex++;
     }
   }
+  swap(array, pivotIndex, end);
+  return pivotIndex;
+}
 
-  export default quicksort;
+const swap = (array, a, b) => {
+  let temp = array[a];
+  array[a] = array[b];
+  array[b] = temp;
+}
 
-  
+export {quicksort, inplace_quicksort};
--- quicksort/index.test.js
+++ quicksort/index.test.js
@@ -1,7 +1,13 @@
-import quicksort from './index.js';
+import {quicksort, inplace_quicksort} from './index.js';
 
-test('test quicksort function', () => {
+test('test quicksort functions', () => {
 
-    expect(quicksort([1, 4, 2, 7, 12, 0, -9, 22]))
-        .toStrictEqual([-9, 0, 1, 2, 4, 7, 12, 22])
+const arr = [1, 4, 2, 7, 12, 0, -9, 22];
+const sortedArr = [-9, 0, 1, 2, 4, 7, 12, 22];
+
+    expect(quicksort(arr))
+        .toStrictEqual(sortedArr);
+
+    expect(inplace_quicksort(arr, 0, arr.length - 1))
+        .toStrictEqual(sortedArr);
 })
